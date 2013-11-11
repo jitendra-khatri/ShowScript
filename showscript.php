12162f33f631ef54f31a015457c2b4432db3db46
Jitendra <jkhatri6@gmail.com>
2013-11-11 15:35:03 +0530

    new file:   index.html 	new file:   plg_showscript.xml 	new file:   showscript.php

---
+++ showscript.php
@@ -0,0 +1,17 @@
+<?php
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+ 
+jimport( 'joomla.plugin.plugin' );
+ 
+/**
+ * Show Script system plugin
+ * Author : Jitendra Khatri
+ */
+class plgSystemShowScript extends JPlugin
+{
+	function onBeforeRender()
+	{
+		?>
+	<?php }
+}?>
