8e68455a8b862df62f2dcfb2811ed655a6d2b70b
root <root@localhost.(none)>
2013-11-12 22:53:03 +0530

    Rename XML File, Added Proper Comments, Added Script loading from URL, Restriction applied for loading scripts only at forntend.

--- showscript.php
+++ showscript.php
@@ -10,8 +10,31 @@ jimport( 'joomla.plugin.plugin' );
  */
 class plgSystemShowScript extends JPlugin
 {
+	// Works just before Joomla framework Render Content.
 	function onBeforeRender()
 	{
-		?>
-	<?php }
+		// Gets Site App.
+		$app	= JFactory::getApplication();
+		
+		// Admin-End then do Nothing 
+		if($app->isAdmin()){
+			return false;
+		}
+		
+		// Gets Plugin Params
+		$params = $this->params;
+		
+		// Gets Script to add on Document
+		$scriptCode = $params->get('script_code');
+		$scriptURL	= $params->get('script_url');
+		$document = JFactory::getDocument();
+		
+		// Add Script on Document.
+		$document->addScriptDeclaration($scriptCode);
+		
+		// Loads Script Form the URL
+		$document->addScript("$scriptURL");
+		
+		return true;		
+	}
 }?>
